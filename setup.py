65bb6362faffcb4e740ef16a5bed903561338479
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2019-04-23 09:58:42 +0200

    release 0.2.8

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
         name='scons-tool-clangpp',
-        version='0.2.7',
+        version='0.2.8',
         package_dir={'sconstool.clangpp': '.'},
         packages=['sconstool.clangpp'],
         namespace_packages=['sconstool'],
